facd73a8add757a0713d91af8a846ad470a11b14
Jamie Hall <hall.jamie@gmail.com>
2019-02-26 17:27:31 -0500

    MVP complete

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+// https://github.com/JamieHall1962/auth-i/pull/1
+
 const express = require("express");
 
-
 const server = express();
+
+
 
 server.use(express.json());
 
 
-const usersRoutes = require("./users/usersRoutes");
+
+const usersRoutes = require("./users/router");
 
 server.use("/api", usersRoutes);
 
-server.listen(4000, () => console.log("\nrunning on port 3300\n"));
+port=4000;
+
+server.listen({port}, () => console.log(`\nAPI is now running on port ${port}\n`));
